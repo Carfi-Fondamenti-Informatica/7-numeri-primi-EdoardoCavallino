97ab034382a31834d5010099d8853633b6b057dc
EdoardoCavallino <91631725+EdoardoCavallino@users.noreply.github.com>
2021-11-29 11:32:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "libreria.h"
 
-int main(){
-  return 0;
+using namespace std;
+
+
+int main() {
+    int x;
+    cin >> x;
+    if(nprimo(x-1,x) == 0){
+        cout << "numero non primo" << endl;
+    }else{
+        cout << "numero primo" << endl;
+    }
+
+    return 0;
 }
